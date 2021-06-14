8c2d88adefef02e1ff296e20f39e6e0ae0440376
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-14 17:29:51 +0200

    Create first variable

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,7 +6,12 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            int x, y, z;
+            x = 4;
+            y = 5;
+            int sum = x + y;
             Console.WriteLine("Hello World!");
+            Console.WriteLine(sum);
         }
     }
 }
