9e7e42d3873dd1062ec56c3206fb70596b425b5a
KamilAdd-Byte <kamillodzinski09@gmail.com>
2021-06-14 21:52:05 +0200

    Create method with one parametr

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -7,28 +7,46 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            /*
+             * Wywołanie metody w c#
+             */
+
+            GetAppInfo();
+
+            /*
+             *  Dane od użytkownika. Przypisanie zmiennej do wyniku działania metody.
+             */
+            string nameInput =  GetUserName();
+
+            GreetUser(nameInput);
+         
+        }
+
+        static void GetAppInfo()
+        {
             string appName = "Guesser";
             int appVersion = 1;
             string appAuthor = "Kamil";
-
             Console.ForegroundColor = ConsoleColor.Yellow;
-
             Console.WriteLine($"[{appName}] Wersja: 0.0.{appVersion}, Author: {appAuthor} ");
-
             Console.ResetColor();
+        }
 
-            /*
-             Dane od użytkownieka
-             */
-
+        static string GetUserName()
+        {
             Console.WriteLine("Jam masz na imię?");
             string nameInput = Console.ReadLine();
+            return nameInput;
+        }
 
+        static void GreetUser(string nameInput)
+        {
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.WriteLine($"Powodzenia {nameInput}, odgadnij liczbę....");
 
             Console.ResetColor();
         }
+
     }
 }
